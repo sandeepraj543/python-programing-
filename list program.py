0882ed332ef6f16353c6476f82a19d61d038392f
sandeepraj543 <sandeepkb805124@gmail.com>
2025-12-29 15:13:05 +0530

    edit file name

---
+++ list program.py
@@ -0,0 +1,6 @@
+movie_list1 = input("Enter your 1st favourite movie name: ")
+movie_list2 = input("Enter your 2nd favourite movie name: ")
+movie_list3 = input("Enter your 3rd favourite movie name: ")
+
+fav_movie_list = [movie_list1,movie_list2,movie_list3] 
+print("your favourite movie list are :-",fav_movie_list)
